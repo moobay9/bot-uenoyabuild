3687f5a1e07c0b2d813221221af997f3c027feed
Masanori Oobayashi <moobay9@gmail.com>
2017-05-25 20:15:08 +0900

    改行コード調整

--- macs.sh
+++ macs.sh
@@ -30,8 +30,8 @@ close
 while read line
 do
     TMP_LINE=`echo ${line} | sed -e "s//,/g"`
-    MACS=${MACS}${TMP_LINE}done < /tmp/.macs.txt
-
+    MACS=${MACS}${TMP_LINE}
+done < /tmp/.macs.txt
 MACS=`echo ${MACS} | sed -e "s/,$//g"`
 
 curl http://${REDIS}:8080/orca/${MACS}/ > /dev/null 2>&1
