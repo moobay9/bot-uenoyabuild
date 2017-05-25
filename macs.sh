22b9ce0c3ce10b49e7eef81448777cb13b06f419
Masanori Oobayashi <moobay9@gmail.com>
2017-05-25 20:11:54 +0900

    スクリプト修正

--- macs.sh
+++ macs.sh
@@ -29,8 +29,7 @@ close
 
 while read line
 do
-    TMP_LINE=`echo ${line} | sed -e "s//,/g"`
-    MACS=${MACS}${TMP_LINE}
+    TMP_LINE=`echo ${line} | sed -e "s/$/,/g"`    MACS=${MACS}${TMP_LINE}
 done < /tmp/.macs.txt
 
 MACS=`echo ${MACS} | sed -e "s/,$//g"`
